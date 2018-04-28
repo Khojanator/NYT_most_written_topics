1cf1e00133942c6577293a27af7b9f2475cbbe98
Ahsan Ali Khoja <aakhoja15@earlham.edu>
2018-04-28 16:40:13 -0400

    The script now takes output file name, and outputs 'keyword~-~url' for each keyword in each article

--- getNYTarticleData.py
+++ getNYTarticleData.py
@@ -13,6 +13,7 @@ def argument_handler():
 
 	parser.add_argument('-b', '--begin_date', help='Enter the beginning date for date range in YYYYMMDD format', default="20180101")
 	parser.add_argument('-e', '--end_date', help='Enter the ending date for date range in YYYYMMDD format', default="20180425")
+	parser.add_argument('-o', '--output_file', help='Enter the name of the output file', default="OutTestData.txt")
 
 	return parser.parse_args()
 
@@ -26,7 +27,7 @@ def is_date(date_str):
 def proper_date(begin_date, end_date):
 	return not((eval(end_date) <= eval(begin_date)) or not(is_date(end_date)) or not(is_date(begin_date)) or not(DateRangeStart <= eval(begin_date) <= DateRangeEndYear) or not(DateRangeStart <= eval(end_date) <= DateRangeEndYear))
 
-def getNYTdata(begin_date, end_date):
+def getNYTdata(begin_date, end_date, outfile):
 	data_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
 	params = {
 		'api-key' : API_KEY,
@@ -37,15 +38,21 @@ def getNYTdata(begin_date, end_date):
 	response =  requests.get(data_url, params=params)
 	content = response.content.decode("utf-8")
 	json_list = json.loads(content)["response"]["docs"]
-	for item in json_list:
-		print(item["keywords"], item["web_url"])
+	for json_obj in json_list:
+		obj_url = json_obj["web_url"]
+		obj_keywords = json_obj["keywords"]
+		if obj_keywords:
+			for item in obj_keywords:
+				print("{0}~-~{1}".format(item['value'], obj_url), file=outfile)
 
 if __name__ == '__main__':
 	args = argument_handler()
 	begin_date = args.begin_date
 	end_date = args.end_date
+	output_file = args.output_file
 
+	file_writer = open(output_file, "w")
 	if proper_date(begin_date, end_date):
-		getNYTdata(begin_date, end_date)
+		getNYTdata(begin_date, end_date, file_writer)
 	else:
 		print("The dates entered are invalid. Program is exiting.")
