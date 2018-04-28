4831798ca0a0058670731d13562d8dff6c97ec40
Ahsan Ali Khoja <aakhoja15@earlham.edu>
2018-04-28 15:36:03 -0400

    Input processing and Argument handling implemented

--- getNYTarticleData.py
+++ getNYTarticleData.py
@@ -1,13 +1,37 @@
 import requests
 import json
 from nyt_api_key import API_KEY
+import argparse
+from dateutil.parser import parse
 
-def getNYTdata():
+USE = "This program gets NYT data for a particular data range with keywords of the article and the web_url.\n Example: user$ python getNYTarticleData.py -b YYYYMMDD -e YYYYMMDD"
+DateRangeStart = 20150101
+DateRangeEndYear = 20180425
+
+def argument_handler():
+	parser = argparse.ArgumentParser(description=USE, formatter_class=argparse.RawTextHelpFormatter)
+
+	parser.add_argument('-b', '--begin_date', help='Enter the beginning date for date range in YYYYMMDD format', default="20180101")
+	parser.add_argument('-e', '--end_date', help='Enter the ending date for date range in YYYYMMDD format', default="20180425")
+
+	return parser.parse_args()
+
+def is_date(date_str):
+	try:
+		x = parse(date_str)
+		return True
+	except:
+		return False 
+
+def proper_date(begin_date, end_date):
+	return not((eval(end_date) <= eval(begin_date)) or not(is_date(end_date)) or not(is_date(begin_date)) or not(DateRangeStart <= eval(begin_date) <= DateRangeEndYear) or not(DateRangeStart <= eval(end_date) <= DateRangeEndYear))
+
+def getNYTdata(begin_date, end_date):
 	data_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
 	params = {
 		'api-key' : API_KEY,
-		'begin_date': "20180326",
-		'end_date': "20180426"
+		'begin_date': begin_date,
+		'end_date': end_date
 	}
 
 	response =  requests.get(data_url, params=params)
@@ -17,4 +41,11 @@ def getNYTdata():
 		print(item["keywords"], item["web_url"])
 
 if __name__ == '__main__':
-	getNYTdata()
+	args = argument_handler()
+	begin_date = args.begin_date
+	end_date = args.end_date
+
+	if proper_date(begin_date, end_date):
+		getNYTdata(begin_date, end_date)
+	else:
+		print("The dates entered are invalid. Program is exiting.")
