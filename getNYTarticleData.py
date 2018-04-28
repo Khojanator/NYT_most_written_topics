b16f3c817538b9240a758bf74e6de147c42d09cf
Ahsan Ali Khoja <aakhoja15@earlham.edu>
2018-04-28 17:35:54 -0400

    The script now makes 100 requests (one per sec - API limit), and processes the received data

--- getNYTarticleData.py
+++ getNYTarticleData.py
@@ -3,6 +3,7 @@ import json
 from nyt_api_key import API_KEY
 import argparse
 from dateutil.parser import parse
+from time import sleep
 
 USE = "This program gets NYT data for a particular data range with keywords of the article and the web_url.\n Example: user$ python getNYTarticleData.py -b YYYYMMDD -e YYYYMMDD"
 DateRangeStart = 20150101
@@ -28,22 +29,25 @@ def proper_date(begin_date, end_date):
 	return not((eval(end_date) <= eval(begin_date)) or not(is_date(end_date)) or not(is_date(begin_date)) or not(DateRangeStart <= eval(begin_date) <= DateRangeEndYear) or not(DateRangeStart <= eval(end_date) <= DateRangeEndYear))
 
 def getNYTdata(begin_date, end_date, outfile):
-	data_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-	params = {
-		'api-key' : API_KEY,
-		'begin_date': begin_date,
-		'end_date': end_date
-	}
+	for page_num in range(100):
+		data_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+		params = {
+			'api-key' : API_KEY,
+			'begin_date': begin_date,
+			'end_date': end_date,
+			'page': page_num
+		}
 
-	response =  requests.get(data_url, params=params)
-	content = response.content.decode("utf-8")
-	json_list = json.loads(content)["response"]["docs"]
-	for json_obj in json_list:
-		obj_url = json_obj["web_url"]
-		obj_keywords = json_obj["keywords"]
-		if obj_keywords:
-			for item in obj_keywords:
-				print("{0}~-~{1}".format(item['value'], obj_url), file=outfile)
+		response =  requests.get(data_url, params=params)
+		content = response.content.decode("utf-8")
+		json_list = json.loads(content)["response"]["docs"]
+		for json_obj in json_list:
+			obj_url = json_obj["web_url"]
+			obj_keywords = json_obj["keywords"]
+			if obj_keywords:
+				for item in obj_keywords:
+					print("{0}~-~{1}".format(item['value'], obj_url), file=outfile)
+		sleep(1)
 
 if __name__ == '__main__':
 	args = argument_handler()
